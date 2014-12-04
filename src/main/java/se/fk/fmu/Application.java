51339e09c93c945409525d36dc33fde222e19ba6
Rasheed Amir <rasheed.amir@live.com>
2014-12-04 17:03:30 +0100

    added webservice

---
+++ src/main/java/se/fk/fmu/Application.java
@@ -0,0 +1,17 @@
+package se.fk.fmu;
+
+/**
+ * Created by Rasheed on 12/4/14.
+ */
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/se/fk/fmu/WebServiceConfig.java
@@ -0,0 +1,48 @@
+package se.fk.fmu;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+/**
+ * Created by Rasheed on 10/25/14.
+ */
+@EnableWs
+@Configuration
+@Slf4j
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean webServiceDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+    @Bean(name = "bestallare-ws")
+    public DefaultWsdl11Definition bestallareWsdl11Definition(XsdSchema bestallareSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("BestallarePort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://fk.ws/fmu/admin/eavrop");
+        wsdl11Definition.setSchema(bestallareSchema);
+        log.info("Registered fk webservice");
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema bestallareSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("ws/bestallare.xsd"));
+    }
+
+}
